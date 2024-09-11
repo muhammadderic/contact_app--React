590f01fedc04e521e6afd84329ac7a54d3123855
muhammadderic <muhammad.donny.e@gmail.com>
2024-09-11 18:33:47 +0700

    Initialize student management app backend with java-springboot

---
+++ student-management-app/be-java-sb/src/main/java/com/github/be_java_sb/BeJavaSbApplication.java
@@ -0,0 +1,13 @@
+package com.github.be_java_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaSbApplication.class, args);
+	}
+
+}
---
+++ student-management-app/be-java-sb/src/test/java/com/github/be_java_sb/BeJavaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.be_java_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
